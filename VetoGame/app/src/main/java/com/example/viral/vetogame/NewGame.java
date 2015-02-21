6ca5d931f578826f350fe9a336de2d6e197c7b35
Eunki Kim <erink807@gmail.com>
2015-02-21 11:31:26 -0500

    add date picker, time picker

--- VetoGame/app/src/main/java/com/example/viral/vetogame/NewGame.java
+++ VetoGame/app/src/main/java/com/example/viral/vetogame/NewGame.java
@@ -7,10 +7,31 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 
+import com.android.datetimepicker.date.DatePickerDialog;
+import com.android.datetimepicker.time.RadialPickerLayout;
+import com.android.datetimepicker.time.TimePickerDialog;
 
-public class NewGame extends Activity {
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+
+public class NewGame extends Activity implements DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener{
+
+    private static final String TIME_PATTERN = "HH:mm";
+
+    private Button btnEventDate;
+    private Button btnEventTime;
+    private Button btnLimitDate;
+    private Button btnLimitTime;
+    private Calendar calendar;
+    private DateFormat dateFormat;
+    private SimpleDateFormat timeFormat;
+    private int id = -1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +46,15 @@ public class NewGame extends Activity {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         // Apply the adapter to the spinner
         spinner.setAdapter(adapter);
+
+        calendar = Calendar.getInstance();
+        dateFormat = DateFormat.getDateInstance(DateFormat.LONG, Locale.getDefault());
+        timeFormat = new SimpleDateFormat(TIME_PATTERN, Locale.getDefault());
+
+        btnEventDate = (Button) findViewById(R.id.btn_event_date);
+        btnEventTime = (Button) findViewById(R.id.btn_event_time);
+        btnLimitDate = (Button) findViewById(R.id.btn_limit_date);
+        btnLimitTime = (Button) findViewById(R.id.btn_limit_time);
 
         findViewById(R.id.btn_invite_people).setOnClickListener(
                 new View.OnClickListener() {
@@ -47,8 +77,49 @@ public class NewGame extends Activity {
 
                     }
                 });
+
+        update();
     }
 
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_event_date:
+                showDatePickerDialog(view);
+                id = 0;
+                break;
+            case R.id.btn_event_time:
+                showTimePickerDialog(view);
+                id = 1;
+                break;
+            case R.id.btn_limit_date:
+                showDatePickerDialog(view);
+                id = 2;
+                break;
+            case R.id.btn_limit_time:
+                showTimePickerDialog(view);
+                id = 3;
+                break;
+        }
+    }
+
+    public void showDatePickerDialog(View v){
+        DatePickerDialog.newInstance(this, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH)).show(getFragmentManager(), "datePicker");
+    }
+
+    public void showTimePickerDialog(View v){
+        TimePickerDialog.newInstance(this, calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE), true).show(getFragmentManager(), "timePicker");
+     }
+
+    private void update() {
+        if(id==-1 || id==0)
+            btnEventDate.setText(dateFormat.format(calendar.getTime()));
+        if(id==-1 || id==1)
+            btnEventTime.setText(timeFormat.format(calendar.getTime()));
+        if(id==-1 || id==2)
+            btnLimitDate.setText(dateFormat.format(calendar.getTime()));
+        if(id==-1 || id==3)
+            btnLimitTime.setText(timeFormat.format(calendar.getTime()));
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -70,5 +141,20 @@ public class NewGame extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onDateSet(DatePickerDialog dialog, int year, int monthOfYear, int dayOfMonth) {
+        calendar.set(year, monthOfYear, dayOfMonth);
+
+        update();
+    }
+
+    @Override
+    public void onTimeSet(RadialPickerLayout view, int hourOfDay, int minute) {
+        calendar.set(Calendar.HOUR_OF_DAY, hourOfDay);
+        calendar.set(Calendar.MINUTE, minute);
+
+        update();
     }
 }
