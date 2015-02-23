b17b325cc3d36efdb09850413862ccf79bde7225
viral7 <viral9793@gmail.com>
2015-02-23 02:31:22 -0500

    Posibbly fixed the error with the game info going of the screen. Also the app now calculates the time remaining before a game ends

--- VetoGame/app/src/main/java/com/example/viral/vetogame/GameAdapter.java
+++ VetoGame/app/src/main/java/com/example/viral/vetogame/GameAdapter.java
@@ -7,8 +7,11 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.TextView;
 
+import java.text.DateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Collection;
+import java.util.Locale;
 
 /**
  * Created by Viral on 2/19/2015.
@@ -17,6 +20,7 @@ public class GameAdapter extends BaseAdapter {
 
     private LayoutInflater inflater;
     private ArrayList<Game> games;
+    private Calendar calendar = Calendar.getInstance();
 
     public GameAdapter(Context context) {
         this.inflater = LayoutInflater.from(context);
@@ -57,6 +61,29 @@ public class GameAdapter extends BaseAdapter {
         return view;
     }
 
+    public String calcTimeRemaining (Calendar start, Calendar end )
+    {
+        long startTime = start.getTimeInMillis();
+        long endTime = end.getTimeInMillis();
+        long diffTime = endTime - startTime;
+        // divide the milliseconds by # of milliseconds in a day to get days difference
+        int days = (int)( diffTime / (1000 * 60 * 60 * 24) );
+        int hours = (int)( diffTime / (1000 * 60 * 60) );
+        int minutes = (int)( diffTime / (1000 * 60) );
+        int seconds = (int)( diffTime / (1000 * 60) );
+        if(days > 0){
+            return(""+days+" d");
+        }else if(hours > 0){
+            return(""+hours+" h");
+        }else if(minutes > 0){
+            return(""+minutes+" m");
+        }else if(seconds > 0){
+            return(""+seconds+" s");
+        }else{
+            return("game over");
+        }
+    }
+
     private void bind(Game game, View view) {
         ViewHolder holder = (ViewHolder) view.getTag();
         if(holder == null){
@@ -67,7 +94,8 @@ public class GameAdapter extends BaseAdapter {
         holder.gameNameTextView.setText(game.getGameName());
         holder.currentSuggestionTextView.setText(game.getCurrentSuggestion().toString());
         //TODO: get current time and subtract from end time.
-        holder.timeRemainingTextView.setText("end time");//""+(game.getTimeEnding().getTime()));
+        String timeRemaining = calcTimeRemaining(calendar,game.getTimeEnding());
+        holder.timeRemainingTextView.setText(timeRemaining);//""+(game.getTimeEnding().getTime()));
         holder.numberOfMembersTextView.setText(""+game.getNumberOfMembers());
     }
 
