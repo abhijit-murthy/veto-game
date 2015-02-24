680cd317c6dd16a1a760957ab4027f09892f9787
Abhijit Murthy <abhijit.murthy@gatech.edu>
2015-02-24 00:06:14 -0500

    Added apidoc for latest api commands

--- controllers/game_data.js
+++ controllers/game_data.js
@@ -4,7 +4,26 @@
 
 var restify = require('restify')
   , db = require('../db');
+/**
+	@api {post} /game_data/create Create a new Game
+	@apiDescription Creates a new Game and adds the creating User to it.
+	@apiName CreateGame
+	@apiGroup Game
 
+	@apiParam {String} user_id  	Creating User's Facebook ID
+	@apiParam {String} event_type 	Type of event. Must be one of 'dinner','lunch, or 'breakfast'
+	@apiParam {Integer} suggestion_ttl 	Time to Live of a suggestion before the game ends (in minutes)
+	@apiParam {Location} center 	Center of the circle in which we are filtering suggestions
+	@apiParam {Integer} radius 	Radius of the circle in which we are filtering suggestions
+
+	@apiSuccess (200) {Integer} game_id 	The ID of the newly created game
+
+	@apiError InvalidArgumentError Bad User ID
+	@apiError InvalidArgumentError Bad Event Type
+	@apiError InvalidArgumentError No Suggestion TTL
+	@apiError InvalidArgumentError No Center
+	@apiError InvalidArgumentError No Radius
+*/
 function createGame(req,res,next){
 	console.log(req.params.user_id);
 	db.getUser(req.params.user_id,function(user){
@@ -43,6 +62,23 @@ function createGame(req,res,next){
 }
 exports.createGame = createGame;
 
+/**
+	@api {get} /game_data/:id Get Game information
+	@apiName GetGameData
+	@apiGroup Game
+
+	@apiParam {String} id 	Game ID
+
+	@apiSuccess {String} id  	Creating User's Facebook ID
+	@apiSuccess {String} event_type 	Type of event. Must be one of 'dinner','lunch, or 'breakfast'
+	@apiSuccess {DateTime} eventTime 	The time of the event 
+	@apiSuccess {DateTime} timeEnding 	Time the game ends
+	@apiSuccess {Integer} suggestion_ttl 	Time to Live of a suggestion before the game ends (in minutes)
+	@apiSuccess {Location} center 	Center of the circle in which we are filtering suggestions
+	@apiSuccess {Integer} radius 	Radius of the circle in which we are filtering suggestions
+
+	@apiError InvalidArgumentError Bad Game ID 
+*/
 function getGameData(req,res,next){
 	var gameId = req.params.id;
 	db.getGame(gameId,function(game){
--- controllers/user_data.js
+++ controllers/user_data.js
@@ -1,10 +1,23 @@
 /*global module:true, require:true, console:true, process:true */
 
+
+
 'use strict';
 
 var restify = require('restify')
   , db = require('../db');
+/**
+	@api {post} /user_data/create Create a new User
+	@apiName CreateUser
+	@apiGroup User
 
+	@apiParam {String} user_id 	Users Facebook ID (remember to get this from the Facebook API)
+	@apiParam {String} name Users Name
+
+	@apiSuccess (200)	Just the HTTP 200 Response
+
+	@apiError InvalidArgumentError User already exists
+*/
 function createUser(req,res,next){
 	db.getUser(req.params.user_id,function(user){
 		if(user != null){
@@ -20,6 +33,21 @@ function createUser(req,res,next){
 }
 exports.createUser = createUser;
 
+/**
+	@api {get} /user_data/:id Get User information
+	@apiName GetUserData
+	@apiGroup User
+
+	@apiParam {String} id 	Users Facebook ID
+
+	@apiSuccess {String} id 	Users Facebook ID
+	@apiSuccess {String} name 	Users Name
+	@apiSuccess {Integer} wins	Number of games the User has won
+	@apiSuccess {Integer} points 	Number of points the User has accumulated
+	@apiSuccess {Integer} extras 	Number of extra vetoes they have accumulated
+
+	@apiError InvalidArgumentError Thrown when the user queried does not exist 
+*/
 function getUserData(req,res,next){
 	db.getUser(req.params.id,function(user){
 		if(user != null){
